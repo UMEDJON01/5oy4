f94dc817fc4a53f258a9e25ed7109d214458fb82
umedjon01 <najottalimfn15@gmail.com>
2024-03-19 01:42:20 +0500

    Homework

---
+++ app.js
@@ -0,0 +1,43 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ol = document.querySelector("ol");
+
+const ClearAllbtn = document.querySelector("#clear-all");
+
+ClearAllbtn.addEventListener(`click`, () => {
+  ol.innerHTML = "";
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  ol.innerHTML += ` <li>${input.value}</li>`;
+
+  input.value = "";
+
+  const music = new Audio("./music.mp3");
+  music.play();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const yourName = document.getElementById("yourName");
+
+  setInterval(function () {
+    const randomColor = getRandomColor();
+    yourName.style.color = randomColor;
+  }, 1000);
+
+  yourName.addEventListener("click", function () {
+    const randomColor = getRandomColor();
+    yourName.style.color = randomColor;
+  });
+
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+});
